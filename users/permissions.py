ea0c1fd55926475dbfd47fcedf7754cf231858af
Kseniya Kuzina <kolosova_ka@mail.ru>
2026-03-20 21:17:32 +0300

    Создана модель подписки Subscription

--- users/permissions.py
+++ users/permissions.py
@@ -22,6 +22,3 @@ class IsOwner(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user
-
-        #     return True
-        # return False
