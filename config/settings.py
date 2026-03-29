fd69ffefd1d0ad7f9a1a951b3f137982ff7e0282
Kseniya Kuzina <kolosova_ka@mail.ru>
2026-03-29 21:18:01 +0300

    Реализована фоновая задача по блокировке пользователя

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "users",
     "materials",
-    'django_celery_beat',
+    "django_celery_beat",
 ]
 
 MIDDLEWARE = [
@@ -147,6 +147,13 @@ if CACHE_ENABLED:
         }
     }
 
+CELERY_BEAT_SCHEDULE = {
+    "deactivate_users": {
+        "task": "materials.tasks.deactivate_users",
+        "schedule": timedelta(minutes=1),
+    },
+}
+
 CELERY_TIMEZONE = TIME_ZONE
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
@@ -154,4 +161,6 @@ CELERY_TASK_TIME_LIMIT = 30 * 60
 CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
 CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
--- materials/tasks.py
+++ materials/tasks.py
@@ -1,7 +1,16 @@
+import logging
+from datetime import timedelta
+
 from celery import shared_task
 from django.core.mail import send_mail
 from django.conf import settings
+from django.utils import timezone
+
+
+from users.models import User
 from .models import Subscription
+
+logger = logging.getLogger(__name__)
 
 
 @shared_task
@@ -9,7 +18,9 @@ def send_course_update_email(course_id, course_title):
     """Асинхронная отправка писем подписчикам курса"""
 
     # Получаем всех подписчиков курса
-    subscriptions = Subscription.objects.filter(course_id=course_id).select_related('user')
+    subscriptions = Subscription.objects.filter(course_id=course_id).select_related(
+        "user"
+    )
 
     if not subscriptions.exists():
         return f"Нет подписчиков у курса {course_title}"
@@ -41,6 +52,30 @@ def send_course_update_email(course_id, course_title):
             recipient_list=recipient_list,
             fail_silently=False,
         )
-        return f"Письма отправлены {len(recipient_list)} подписчикам курса {course_title}"
+        return (
+            f"Письма отправлены {len(recipient_list)} подписчикам курса {course_title}"
+        )
     except Exception as e:
         return f"Ошибка отправки: {str(e)}"
+
+
+@shared_task
+def deactivate_users():
+    """Блокировка пользователя"""
+
+    # Получаем всех пользователей с вильтром по is_active
+    user = User.objects.filter(is_active=True)
+
+    for u in user:
+        if timezone.now() - u.last_login > timedelta(days=30):
+            u.is_active = False
+            u.save()
+            # Добавляем логирование
+            logger.info(f"Пользователь {u.username} заблокирован.")
+            # Отправляем уведомление администратору
+            send_mail(
+                "Блокировка пользователя",
+                f"Пользователь {u.username} был заблокирован, поскольку не заходил на платформу более 30 дней",
+                settings.DEFAULT_FROM_EMAIL,
+                ["ksyu_student_2026@mail.ru"],
+            )
