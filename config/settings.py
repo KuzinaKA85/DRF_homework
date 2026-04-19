0adac9b1f465a522151e543ec65ce77ecc5b9e6b
Kseniya Kuzina <kolosova_ka@mail.ru>
2026-04-19 23:16:03 +0300

    Add GitHub Actions workflow for CI/CD

--- config/settings.py
+++ config/settings.py
@@ -76,12 +76,10 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 if "test" in sys.argv:
-    SECRET_KEY = "test-secret-key"
-    DEBUG = True
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "test_db.sqlite3",
+            "NAME": BASE_DIR / "test_db_sqlite3",
         }
     }
 else:
